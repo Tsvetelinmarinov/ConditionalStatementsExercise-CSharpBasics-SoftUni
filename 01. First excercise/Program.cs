48ebdea015b8e03bc65c1f5f59f400fc1ab4b89d
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-09 20:19:06 +0200

    Add project files.

---
+++ 01. First excercise/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Sum seconds
+ */
+
+
+/**
+ * 
+ */
+
+
+int firstRunnerTime = int.Parse(Console.ReadLine());
+int secondRunnerTime = int.Parse(Console.ReadLine());
+int thirdRunnerTime = int.Parse(Console.ReadLine());
+
+
+
+int totalTime = firstRunnerTime + secondRunnerTime + thirdRunnerTime;
+
+
+int minutes = totalTime / 60;
+int seconds = totalTime % 60;
+
+
+if (seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
