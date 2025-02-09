60c6002f1f73ff53500f2ae641d5a6b0c8e4dde4
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-09 22:19:56 +0200

    Задача 3

---
+++ 03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * 03. Time + 15 Minutes
+ */
+
+
+int hour = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+
+minutes += 15;
+
+
+if (minutes > 59)
+{
+    hour++;
+    minutes -= 60;
+}
+
+if (hour > 23)
+{
+    hour -= 24;
+}
+
+
+
+if (minutes < 10)
+{
+    Console.WriteLine($"{hour}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hour}:{minutes}");
+}
